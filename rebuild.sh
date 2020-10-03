30f17b3b4b92df53e9e0f2bddc73f366748ccc92
Cem Aydin <cem.aydin@gmx.ch>
2020-10-03 22:05:37 +0200

    docker restart unless-stopped instead of always

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
 docker build -t botootl .
-docker stop botootl-run
-docker rm botootl-run
-docker run --name botootl-run -d --restart always botootl
+docker stop botootl
+docker rm botootl
+docker run --name botootl -d --restart unless-stopped botootl
